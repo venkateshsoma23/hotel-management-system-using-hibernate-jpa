86241c2366ea50474a91178dc0c75bfe2ae9662f
venkateshsoma23 <vickysoma05@gmail.com>
2025-07-15 17:48:24 +0530

    changed

--- src/test/java/com/venkatesh/AppTest.java
+++ src/test/java/com/venkatesh/AppTest.java
@@ -1,16 +1,11 @@
 package com.venkatesh;
 
-import org.junit.jupiter.api.Test; 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
